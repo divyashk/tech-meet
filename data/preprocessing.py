8ac0c00f4808e3d3fc0031a9009481a710d5f468
Signior-X <b19188@students.iitmandi.ac.in>
2022-03-22 13:07:17 +0530

    data preprocessing code started

---
+++ data/preprocessing.py
@@ -0,0 +1,16 @@
+#%%
+import pandas as pd
+
+
+#%%
+doctor_detail = pd.read_csv("inter_iit_data.xlsx - Doctor Detail.csv")
+doctor_detail
+
+# %%
+hospital_detail = pd.read_csv("inter_iit_data.xlsx - Hospital Data.csv")
+hospital_detail
+
+# %%
+patient_detail = pd.read_csv("inter_iit_data.xlsx - Patient Details.csv")
+patient_detail
+# %%
