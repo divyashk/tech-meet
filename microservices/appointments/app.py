0182bb4006821ff7d31773b8942c420547b00eeb
Vinayak Sachan <vinayakasachan@gmail.com>
2022-03-24 02:09:46 +0530

    new_appointment code remaining,rest done in microservice

--- microservices/appointments/app.py
+++ microservices/appointments/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, json, render_template, jsonify, request, session, flash, redirect, url_for, send_file
+from pymysql import NULL
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import os
 
-# Initialization 
+# Initialization
 cred = credentials.Certificate('../../creds.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -26,13 +27,32 @@ app.secret_key = os.getenv('SECRET_KEY')
         description : "details about disease"
     }
 '''
+
+
 @app.route('/book_appointment', methods=['POST'])
 def book_appointment():
     data = request.json
-    # store this data (also add status = open) in appointments collection and get appointment id
-    # open patients collection, add this appointment id to appointments array []
-    # open doctors collection, add this appointment id to appointments array []
-    return jsonify(success=True)
+    try:
+        # store this data (also add status = open) in appointments collection and get appointment id
+        data['status'] = "open"
+        data['appointment_id'] = data['patientid'] + "_"+data['doctorid']+"_"+data['datetime']
+        db.collection("appointments").document(
+            data['appointment_id']).set(data, merge=True)
+
+        # open patients collection, add this appointment id to appointments array []
+        patient_ref = db.collection("patient").document(data['patientid'])
+        patient_ref.update(
+            {u'appointments': firestore.ArrayUnion([data['appointment_id']])})
+
+        # open doctors collection, add this appointment id to appointments array []
+        doctor_ref = db.collection("doctor").document(data['doctorid'])
+        doctor_ref.update(
+            {u'appointments': firestore.ArrayUnion([data['appointment_id']])})
+
+        return jsonify(success=True)
+    except:
+        return jsonify(success=False)
+
 
 '''
     Desc : List all appointments
@@ -41,13 +61,38 @@ def book_appointment():
         userid : "random_user_id"
     }
 '''
+
+
 @app.route('/show_appointments', methods=['POST'])
 def show_appointments():
     data = request.json
     # fetch the user data from user's collection and identify whether doctor or patient
+    user_data = db.collection("users").document(data['userid']).get().to_dict()
     # depending on that, fetch the required document from doctor's or patient's collection
     # fetch the appointments array, then return the list of appointments whose status is open as json
-    return jsonify(success=True)
+
+    if(user_data['role'] == 0 or user_data['role'] == 1):
+
+        appointments=[]
+        if(user_data['role'] == 0):
+            appointments = db.collection("patient").document(
+                data['userid']).get().to_dict()['appointments']
+        else:
+            appointments = db.collection("doctor").document(
+                data['userid']).get().to_dict()['appointments']
+
+        open_appointments = []
+        for appointment in appointments:
+            app_data = db.collection('appointments').document(
+                appointment).get().to_dict()
+            if app_data['status'] == "open":
+                open_appointments.append(app_data)
+
+        return jsonify(success=True, open_appointments=open_appointments)
+
+    else:
+
+        return jsonify(success=False)
 
 
 '''
@@ -60,12 +105,31 @@ def show_appointments():
         next_appointment : NULL or datetime
     }
 '''
+
+
 @app.route('/close_appointment', methods=['POST'])
 def close_appointment():
     data = request.json
-    # update appointment status as closed and also add the data received
-    # if next_appointment is not null, then create a new appointment as a followup for this
-    return jsonify(success=True)
+    # Have to check doctor in main app.py itself
+    try:
+        # update appointment status as closed and also add the data received
+        current_appointment=db.collection('appointments').document(data['appointment_id'])
+        current_appointment.set({"description": data["description"], "prescription": data["prescription"],"next_appointment": data["next_appointment"], "status": "closed"}, merge=True)
+
+        current_appointment_data=current_appointment.get().to_dict()
+        # if next_appointment is not null, then create a new appointment as a followup for this
+        next_appointment_date=current_appointment_data['next_appointment']
+        if(next_appointment_date is not NULL):
+
+            new_appointment=db.collection('appointment').document(current_appointment_data['doctorid']+"_"+current_appointment_data['patientid']+"_"+next_appointment_date)
+            
+            # modify /book_appointment route below
+            # new_appointment.set({...})
+            #??????????????????????
+        
+        return jsonify(success=True)
+    except:
+        return jsonify(success=False)
 
 # Main
 if __name__ == '__main__':
