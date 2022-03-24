53bf1fa5b5a6eaa107b319f74d2b10e3cb95c076
divyasheel <dk0018w@gmail.com>
2022-03-24 17:05:03 +0530

    Added backup for microservices

---
+++ arch/inventory.py
@@ -0,0 +1,75 @@
+from flask import Flask, json, render_template, jsonify, request, session, flash, redirect, url_for, send_file
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import os
+
+# Initialization 
+cred = credentials.Certificate('creds.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+
+# APIs
+
+'''
+    Desc : Add item to inventory
+    @json 
+    {
+        {
+            id : "some unique"  
+            item_name : "paracetemol",
+            quantity : 10,
+            price : "Rs 100" 
+        },
+        dispensary: "name"
+    }
+'''
+@app.route('/add', methods=['POST'])
+def add_item_to_inv():
+    data = request.json
+    # Add item to inventory for a particular dispensary
+    return True
+
+'''
+    Desc : 
+    @json 
+    {
+        
+        id : "some unique" ,
+        field_to_change : "field", 
+        value : "value",
+        dispensary: "name",
+    }
+'''
+@app.route('/update', methods=['PATCH'])
+def update_item_from_inv():
+    data = request.json
+    # Update an item in inventory for a particular dispensary ( for adjusting price, quantity, etc) 
+    return True
+
+
+'''
+    Desc : Add item to inventory
+    @json 
+    {
+        id : "some unique" ,
+        dispensary: "name"
+    }
+'''
+@app.route('/fetch/:item_name', methods=['GET'])
+def fetch_item_from_inv(item_name):
+    data = request.json
+    # Search for an item in inventory and return
+    return True
+
+# TODO
+@app.route('/hospital/:hosp_id/getall', methods=['POST'])
+def get_all_meds(hosp_id):
+    return True
+
+# Main
+if __name__ == '__main__':
+    app.run(debug=True,  threaded=True , port = 5004)
